dc1255a888ae8f2160c79da4b952b74a28fbaf5b
tiffaphm <tiffanynpham@gmail.com>
2017-08-29 15:12:52 -0700

    Cleaned up code.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,21 +1,6 @@
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function() {
-//     oldStep();
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
-
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('blinky');
-  // this.step();
-  // this.setPosition(top, left);
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
